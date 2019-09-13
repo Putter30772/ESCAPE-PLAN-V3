561a05af93fb2c638de6a043298a12bc6ff588df
Putter30772 <55172008+Putter30772@users.noreply.github.com>
2019-09-13 15:54:42 +0700

    Add files via upload

---
+++ 06-Grid-based movement/Label.gd
@@ -0,0 +1,21 @@
+extends Label
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+# Called when the node enters the scene tree for the first time.
+
+func _ready():
+	pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	set_text(str(variable.turn))
+	add_color_override("font_color", Color(1,1,1,1))
+	if(variable.turn == 0):
+		set_text("PRISONER'S TURN")
+	if(variable.turn == 1):
+		set_text("WARDEN'S TURN")
+	if(variable.winner == 1):
+		set_text("PRISONER WIN")
+	if(variable.winner ==2):
+		set_text("WARDEN WINS")
---
+++ 06-Grid-based movement/Node.gd
@@ -0,0 +1,8 @@
+extends Node
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var turn = 0
+var winner = 0
+var gameState = 0
